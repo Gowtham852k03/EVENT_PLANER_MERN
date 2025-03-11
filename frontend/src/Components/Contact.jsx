e43b2b1efa7a2cc89d1efd4c84bfed816671612a
Your Name <your_email@example.com>
2025-03-11 23:22:59 +0530

    updated

--- frontend/src/Components/Contact.jsx
+++ frontend/src/Components/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
 
     try {
       const res = await axios.post(
-        "https://event-planer-mern.onrender.com/api/v1/message/send",
+        "http://event-planer-mern.onrender.com/api/v1/message/send",
         { name, email, subject, message },
         { headers: { "Content-Type": "application/json" } }
       );
