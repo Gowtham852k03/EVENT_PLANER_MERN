97443c18cd30d360a3f19ccb06e8fb77626e3db2
Your Name <your_email@example.com>
2025-03-11 23:25:45 +0530

    updated

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
     <footer>
       <div className="banner">
-        <div className='title'>
-          <h1>KINGS</h1>
+        <div className="title">
+          <h1>GOWTHAM</h1>
           <p>Event Planner</p>
         </div>
         <div className="tag">
           <label>News Letter</label>
           <div>
-            <input type="email" placeholder='Enter your email' />
+            <input type="email" placeholder="Enter your email" />
             <button>Subscribe</button>
           </div>
           <p>sign up with your email to get latest updates</p>
         </div>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
