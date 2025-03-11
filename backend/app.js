a75122815f45714063fe77a3433af953be458d6a
Your Name <your_email@example.com>
2025-03-11 22:58:16 +0530

    second commit

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,13 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.json({
+        sucess: true,
+        message: "Hello from backend"
+    });
+})
 app.use("/api/v1/message", messageRouter);
 dbconnection();
 
